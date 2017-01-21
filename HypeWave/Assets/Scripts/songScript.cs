717da833b5f184d039300ebde362e764fa0b12a7
DESKTOP-LO495TT\max13 <jrehberg@usc.edu>
2017-01-20 19:50:23 -0800

    More Chiz
    Plz

---
+++ HypeWave/Assets/Scripts/songScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class songScript : MonoBehaviour {
+
+
+    public AudioSource mySong;
+    public float BPM;
+    public float bufferTime;
+
+    public float timeStamp;
+
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
