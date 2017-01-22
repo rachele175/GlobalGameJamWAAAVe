4d53d41ee5fb227bde6cd89500253b03cf361a3b
Matt Levonian <elevonian@gmail.com>
2017-01-22 13:24:42 -0800

    spotlight

---
+++ HypeWave/Assets/Scripts/Spotlight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spotlight : MonoBehaviour {
+    [SerializeField]
+    internal Transform target;
+    private void OnEnable()
+    {
+        if (target != null)
+        {
+            transform.position = target.position + Vector3.back * 2 + Vector3.up * 2 + Vector3.left;
+        }
+    }
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + Vector3.back * 1 + Vector3.up * 1 + Vector3.left * 0.25f,  Time.deltaTime / 0.1f);
+        //transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x, transform.position.y, target.position.z), Time.deltaTime / 0.3f);
+    }
+}
