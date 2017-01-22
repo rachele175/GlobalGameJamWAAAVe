a7851cc3a1632a6395c26ee8ce13d25a3e323b15
DESKTOP-LO495TT\max13 <jrehberg@usc.edu>
2017-01-22 14:39:07 -0800

    Spotlight Coloring

--- HypeWave/Assets/Scripts/playerController.cs
+++ HypeWave/Assets/Scripts/playerController.cs
@@ -51,8 +51,9 @@ public class playerController : MonoBehaviour {
 		myCharacter.transform.rotation = Quaternion.AngleAxis(40f, Vector3.right);
 
         spotlight = Instantiate(spotlightPrefab);
+     
         spotlight.target = this.transform;
-
+        spotlight.assignColor();
         if (!creatingSong)
         {
             myDisplay.noMoreHype += crowdPlayer.Die;
